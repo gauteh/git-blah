fd6c3ba1df58e73af0df25ddedb72bd05779f82e
Gaute Hope <gaute.hope@met.no>
2021-10-05 13:27:00 +0200

    add setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+
+setuptools.setup(
+    name        = 'blah',
+    description = 'blah?',
+    author      = 'Gaute Hope',
+    url         = 'https://github.com/gauteh/git-blah',
+    version = '0.1',
+    license = 'GPLv2',
+    packages = setuptools.find_packages(),
+)
+
